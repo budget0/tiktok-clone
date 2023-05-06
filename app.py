afc2c6937385ee436dab63dd91e65b0328019390
Budget 0 <124797239+budget0@users.noreply.github.com>
2023-05-06 17:20:17 +0930

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import random
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+RAPIDAPI_KEY = '584f9c0d6bmsh5bbe121062eea1ap158bbfjsn0f162809d61a'
+RAPIDAPI_HOST = 'reddit-meme.p.rapidapi.com'
+
+@app.route('/')
+def index():
+    images = get_random_images()
+    return render_template('index.html', images=images)
+
+def get_random_images():
+    url = f"https://reddit-meme.p.rapidapi.com/memes/trending"
+
+    headers = {
+        "X-RapidAPI-Key": RAPIDAPI_KEY,
+        "X-RapidAPI-Host": RAPIDAPI_HOST,
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    images = []
+    for post in data:
+        if post['url'].endswith(('.jpg', '.png', '.jpeg', '.gif')):
+            images.append({'src': {'medium': post['url']}, 'photographer': post['title']})
+
+    # If there are less than 5 images, add duplicates to make it 5
+    while len(images) < 5:
+        images += images
+
+    return images
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
